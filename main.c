1947618b12aea519793a6c7a6fe2405d67f79277
Amandine Demuynck <amdemuyn@c1r3s5.42madrid.com>
2023-11-15 21:06:40 +0100

    small things

--- main.c
+++ main.c
@@ -38,13 +38,27 @@ char	*find_path(char **envp)
 	}
 	return (0);
 }
+/* The `waitpid(-1, NULL, 0);` function is used to wait for any child process to terminate. The first
+parameter `-1` indicates that the function should wait for any child process. The second parameter
+`NULL` is used to discard the exit status of the child process. The third parameter `0` is used to
+specify that the function should block until a child process terminates. */
+void	parent_process(int outfile, char **cmd2, char **envp, int *end)
+{
+	(void)cmd2;
+	(void)envp;
+	printf("waitpid :%d\n", waitpid(-1, NULL, 0));
+	waitpid(-1, NULL, 0);
+	dup2(outfile, STDOUT_FILENO); // outfile is the stdout any output written to stdout will be written to the `outfile` file instead.
+	close(end[1]); //closing the write end of the pipe to indicate that `parent_process` has finished writing. This allows the child process to detect the end of the input and exit.
+	dup2(end[0], STDIN_FILENO); // end[0] is the stdin
+	close(end[0]); //closing the read end of the pipe to indicate that `parent_process` has finished reading. This allows the child process to detect the end of the output and exit.
+	close(outfile); //close fd for the `outfile` file o free up system resources and indicate that the file is no longer needed.
+}
+
 /* The `pipe(end);` function is creating a pipe, which is a mechanism for interprocess communication.
 It creates a pair of file descriptors, `end[0]` and `end[1]`, where `end[0]` is the file descriptor
 for reading from the pipe and `end[1]` is the file descriptor for writing to the pipe. */
-/* The `waitpid(pid, NULL, 0);` function is used to wait for a specific child process to terminate. In
-this case, it is waiting for the child process with the process ID `pid` to terminate. The `NULL`
-parameter is used to discard the exit status of the child process, and the `0` parameter is used to
-specify that the function should block until the child process terminates. */
+
 void	start_process(int infile, int outfile, char **argv, char **envp)
 {
 	int end[2];
@@ -53,15 +67,14 @@ void	start_process(int infile, int outfile, char **argv, char **envp)
 	char	**cmd1;
 	char	**cmd2;
 	(void)infile;
-	(void)outfile;
 
 	path = ft_split(find_path(envp), ':');
 	if (!path)
 		exit_error("Path Error");
-		//printf("%s\n", *path);
+		printf("%s\n", *path);
 	pipe(end);
-	printf("end[0] :%d\n", end[0]);
-	printf("end[1] :%d\n", end[1]);
+	//printf("end[0] :%d\n", end[0]);
+	//printf("end[1] :%d\n", end[1]);
 	pid = fork();
 	cmd1 = ft_split(argv[2], ' ');
 	cmd2 = ft_split(argv[3], ' ');
@@ -74,9 +87,7 @@ void	start_process(int infile, int outfile, char **argv, char **envp)
 		//child_process(infile, cmd1, envp, end);
 	else
 		ft_putstr_fd("parent process\n", 1);
-		//parent_process(outfile, cmd2, envp, end);
-	printf("waitpid :%d\n", waitpid(pid, NULL, 0));
-	waitpid(pid, NULL, 0);
+		parent_process(outfile, cmd2, envp, end);
 }
 
 int	main(int argc, char **argv, char **envp)
