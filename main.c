3091722473257ed9167a2f5c21f65557cbf9287f
calamitymandy <amandined.plateforme@gmail.com>
2023-11-17 18:31:28 +0100

    parent process in doing

--- main.c
+++ main.c
@@ -12,18 +12,6 @@
 
 #include "pipex.h"
 
-/**
- * The function "find_path" searches through an array of strings (envp) to find 
- * the string that starts with "PATH" and returns a pointer to the characters 
- * after the fifth character of that string (after "PATH=").
- * @param envp The parameter `envp` is a pointer to an array of strings. Each 
- * string represents an environment variable in the form "NAME=VALUE". In this 
- * case, the function is searching for the environment variable "PATH" and 
- * returning a pointer to the value part of the variable.
- * @return a pointer to the string that represents the value of the "PATH" 
- * environment variable.
- */
-
 char	*find_path(char **envp, char *cmd)
 {
 	int		i;
